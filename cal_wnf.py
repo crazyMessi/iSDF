fix sdf calculation

cal_wnf.py
@@ -140,7 +140,7 @@ class WNF:
     Winding Number Field 类
     用于计算和管理点云的 winding number 场
     """
-    def __init__(self, points, epsilon=1e-8):
+    def __init__(self, points, epsilon=1e-8,device=None):
         """
         构造函数
         
@@ -148,7 +148,10 @@ class WNF:
             points: (N, 3) array/tensor - 点云坐标
             epsilon: float - 计算 winding number 时防止除零的小量
         """
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        if device is None:
+            self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        else:
+            self.device = device
         
         # 转换输入为 torch tensor
         if not isinstance(points, torch.Tensor):

dataloader.py
@@ -9,6 +9,7 @@ from scipy.spatial import cKDTree
 from tqdm import tqdm  # 导入tqdm进度条库
 import json
 from dtype_utils import get_numpy_dtype, get_torch_dtype, numpy_to_tensor
+from field import MeshSDF
 
 
 
@@ -244,12 +245,12 @@ class QueryPointsDatasetMix(Dataset):
             _, _, mask, faces, vertices = query_points_dataset[i]
             qp = pts[mask==1]   
             self.query_points.append(np.concatenate((qp,np.ones((qp.shape[0],1))*i),axis=1))
-            f = SDF(vertices,faces)
-            sdistane = f(qp)
-            self.sdistane.append(sdistane)
+            # f = SDF(vertices,faces)
+            # sdistane = f(qp)
+            f = MeshSDF(vertices,faces)
+            sdistane = f.query(qp)
+            self.sdistane.append(sdistane)    
             
-            
-        
         
         self.query_points = np.concatenate(self.query_points,axis=0)
         self.sdistane = np.concatenate(self.sdistane,axis=0)

network.py
@@ -48,7 +48,7 @@ class SparseVoxelEncoder(nn.Module):
         
         # 线性层，用于生成最终特征向量
         self.fc = nn.Linear(512, output_dim)
-        
+        self.activation = nn.Sigmoid()
     def forward(self, x):
         """
         输入:
@@ -61,7 +61,7 @@ class SparseVoxelEncoder(nn.Module):
         
         # 通过全连接层生成最终特征向量
         x = self.fc(x)  # x已经是普通Tensor，不需要.features
-        
+        x = self.activation(x)
         return x
     
     @staticmethod
@@ -187,12 +187,13 @@ class ShapeFeatureExtractor(nn.Module):
         self.bn1 = nn.BatchNorm1d(64)
         self.bn2 = nn.BatchNorm1d(128)
         self.bn3 = nn.BatchNorm1d(1024)
+        self.activation = nn.Sigmoid()
         
         
     def forward(self, x):
-        x = F.relu(self.bn1(self.layer1(x)))
-        x = F.relu(self.bn2(self.layer2(x)))
-        x = F.relu(self.bn3(self.layer3(x)))
+        x = self.activation(self.bn1(self.layer1(x)))
+        x = self.activation(self.bn2(self.layer2(x)))
+        x = self.activation(self.bn3(self.layer3(x)))
         
         x = torch.max(x, dim=2, keepdim=True)[0]
         x = x.view(-1, 1024)
@@ -212,4 +213,5 @@ class SDFDecoder(nn.Module):
         x = F.relu(self.layer2(x))
         x = F.relu(self.layer3(x))
         x = self.layer4(x)
+        x = F.sigmoid(x)
         return x

train_mix.py
@@ -176,9 +176,19 @@ def train(config):
             
             sdf_pred = sdf_decoder(qs_feature) # B x 1
             gt_y = sdistane
+            gt_y[gt_y>0] = 1
+            gt_y[gt_y<0] = -1
+
             
             # Compute loss
             loss = nn.MSELoss()(sdf_pred.squeeze(), gt_y.squeeze())
+            P = (sdf_pred>0).squeeze()
+            N = (sdf_pred<0).squeeze()
+            TP = (P & (gt_y>0)).sum()
+            TN = (N & (gt_y<0)).sum()
+            FP = (P & (gt_y<0)).sum()
+            FN = (N & (gt_y>0)).sum()
+            acc = (TP + TN) / (TP + TN + FP + FN)
             
             # Backward pass
             optimizer.zero_grad()
@@ -186,7 +196,7 @@ def train(config):
             optimizer.step()
             
             if batch_idx % config["training"]["log_interval"] == 0:
-                print(f'Epoch: {epoch}, Batch: {batch_idx}, Loss: {loss.item():.6f}')
+                print(f'Epoch: {epoch}, Batch: {batch_idx}, Loss: {loss.item():.6f}, Acc: {acc:.6f}')
                 
         # Save checkpoint
         if (epoch + 1) % config["training"]["save_interval"] == 0: